c759b807ac29312bb8e10dc25fa80ee24fd8fa59
lisyce <lisyce@users.noreply.github.com>
2020-12-23 09:34:20 -0800

    Renaming folders/files

--- code/core/urls.py
+++ code/core/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('website.urls', namespace='website')),
+    path('', include('thread.urls', namespace='thread')),
 ]
--- code/thread/urls.py
+++ code/thread/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-app_name = 'website'
+app_name = 'thread'
 
 urlpatterns = [
     path('', views.home, name='homepage'),
