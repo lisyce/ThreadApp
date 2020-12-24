b8fa9806af68f1998887c3d15defe544fe43b3a4
lisyce <lisyce@users.noreply.github.com>
2020-12-23 21:17:55 -0800

    added thread model

--- code/thread/models.py
+++ code/thread/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class Thread (models.Model):
+    
+    STYLES = (
+        ('embroidery floss', 'Embroidery Floss'),
+        ('craft cord', 'Craft Cord'),
+        ('waxed cord', 'Waxed Cord'),
+    )
+
+    brand = models.CharField(max_length=200)
+    style = models.CharField(max_length=200, choices=STYLES)
+    #TODO make this validate numbers above 0
+    id_number = models.IntegerField()
+    color_name = models.CharField(max_length=250)
+    length_owned = models.DecimalField(max_digits=50, decimal_places=2)
+    added_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return ('{0} {1} #{2}: {3}. {4} meters owned.'.format(self.brand, self.style, self.id_number, self.color_name, self.length_owned))    
+ 
