42a76a84d60c0cd47b583867f687a24d6c49da83
lisyce <lisyce@users.noreply.github.com>
2020-12-26 13:24:39 -0800

    added collection model

--- code/thread/admin.py
+++ code/thread/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from . import models
 
 # Register your models here
-class AuthorAdmin(admin.ModelAdmin):
+class ThreadAdmin(admin.ModelAdmin):
     list_display = ('brand', 'id_number', 'style', 'color_name', 'length_owned', 'added_by')
 
-admin.site.register(models.Thread, AuthorAdmin)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'added_by', 'status')
+
+admin.site.register(models.Thread, ThreadAdmin)
+admin.site.register(models.Collection, CollectionAdmin)
--- code/thread/models.py
+++ code/thread/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import MinValueValidator
+from django.contrib.auth import get_user_model
+
+def get_deleted_user():
+    return get_user_model().objects.get_or_create(username='deleted')[0]
 
 class Thread (models.Model):
     
@@ -16,7 +19,7 @@ class Thread (models.Model):
     id_number = models.IntegerField()
     color_name = models.CharField(max_length=250)
     length_owned = models.DecimalField(max_digits=50, decimal_places=2)
-    added_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    added_by = models.ForeignKey(User, on_delete=models.SET(get_deleted_user))
 
     class Meta:
         ordering = ('brand',)
@@ -24,3 +27,15 @@ class Thread (models.Model):
     def __str__(self):
         return ('{0} {1} #{2}: {3}. {4} meters owned.'.format(self.brand, self.style, self.id_number, self.color_name, self.length_owned))    
  
+class Collection (models.Model):
+    
+    STATUS = (
+        ('Public', 'Public'),
+        ('Private', 'Private'),
+    )
+    
+    name = models.CharField(max_length=250)
+    description = models.CharField(max_length=400)
+    added_by = models.ForeignKey(User, on_delete=models.SET(get_deleted_user))
+    status = models.CharField(max_length=200, choices=STATUS)
+    threads = models.ManyToManyField(Thread)
